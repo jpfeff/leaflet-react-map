6b24c079c921cef3a164ded710a5e248f6ec531f
Josh Pfefferkorn <pfefferkorjd@gmail.com>
2022-09-06 17:22:43 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+// timer
+let sec = 0;
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${sec} seconds.`);
+  sec += 1;
+}, 1000);
